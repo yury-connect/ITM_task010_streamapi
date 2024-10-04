163a7881d8b679f8c4f98cc58a0dd41586d43893
Yury <yury.connect@gmail.com>
2024-10-04 23:13:02 +0300

    solution: task2

--- Stream API/task2/src/Solution.java
+++ Stream API/task2/src/Solution.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
@@ -17,13 +18,22 @@ public class Solution {
         Task task3 = new Task(3, "Write a mobile application to store my tasks", TaskType.CODING, LocalDate.of(2015, Month.JULY, 3)).addTag("coding").addTag("mobile");
         Task task4 = new Task(4, "Write a blog on Java 8 Streams", TaskType.WRITING, LocalDate.of(2015, Month.JULY, 4)).addTag("blogging").addTag("writing").addTag("streams");
         Task task5 = new Task(5, "Read Domain Driven Design book", TaskType.READING, LocalDate.of(2015, Month.JULY, 5)).addTag("ddd").addTag("books").addTag("reading");
-        List<Task> tasks = Arrays.asList(task1, task2, task3, task4, task5, task5, task5, task3);
+        Task task6 = new Task(6, "Read Domain Driven Design book", TaskType.READING, LocalDate.of(2015, Month.JULY, 5)).addTag("ddd").addTag("books").addTag("reading");
+        List<Task> tasks = Arrays.asList(task1, task2, task3, task4, task5, task5, task5, task3, task6);
 
+        System.out.println("\n\tБазовый перечень задач:");
+        tasks.forEach(System.out::println);
+
+        System.out.println("\n\tПодсказка: Должно вывести задачи в следующем порядке: task1, task2, task5");
+
+        System.out.println("\n\tПеречень задач согласно задания:");
         allReadingTasks(tasks).forEach(System.out::println);
     }
 
     private static List<Task> allReadingTasks(List<Task> tasks) {
-        return null;
-        // Ваш код здесь
+        return tasks.stream()
+                .filter(task -> task.getType() == TaskType.READING)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
--- Stream API/task2/src/Task.java
+++ Stream API/task2/src/Task.java
@@ -1,8 +1,11 @@
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
+
 public class Task {
+
     private final Integer id;
 
     private final String title;
@@ -11,12 +14,13 @@ public class Task {
 
     private final LocalDate createdOn;
 
-
     private boolean done = false;
 
     private Set<String> tags = new HashSet<>();
 
     private LocalDate dueOn;
+
+
 
     public Task(Integer id, String title, TaskType type, LocalDate createdOn) {
         this.id = id;
@@ -25,11 +29,12 @@ public class Task {
         this.createdOn = createdOn;
     }
 
-
     public Task addTag(String tag){
         this.tags.add(tag);
         return this;
     }
+
+
 
     public Integer getId() {
         return id;
@@ -79,5 +84,26 @@ public class Task {
                 ", type=" + type +
                 ", createdOn=" + createdOn +
                 '}';
+    }
+
+
+    // Я взял на себя смелость переопределить equals, т.к. объекты должны сравниваться по полям, а не по ссылкам.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // Быстрая проверка на ссылку
+        if (o == null || getClass() != o.getClass()) return false; // Проверка на тип объекта
+        Task task = (Task) o;
+        return
+                // Поле id НЕ сравниваем, т.к. это динамически назначаемое значение
+                Objects.equals(title, task.title) && // Сравниваем поле title
+                type == task.type && // Сравниваем поле type (перечисление)
+                Objects.equals(createdOn, task.createdOn) && // Сравниваем поле createdOn
+                Objects.equals(tags, task.tags); // Сравниваем множества tags // Метод Objects.equals() корректно сравнивает множества, так как он вызывает метод equals() у самого множества, который, в свою очередь, проверяет равенство элементов независимо от их порядка.
+    }
+
+    @Override
+    public int hashCode() {
+        // Поле id НЕ участвует в hashCode, т.к. оно не участвует и в equals
+        return Objects.hash(title, type, createdOn, tags); // Учитываем все поля, включая tags
     }
 }
